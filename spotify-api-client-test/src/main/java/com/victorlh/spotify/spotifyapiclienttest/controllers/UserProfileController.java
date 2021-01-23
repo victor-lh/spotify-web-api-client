1ba5478bd391f746c7ce78f114f509cf136ead7d
Victor LH <victor.latorre@victorlh.es>
2021-01-23 12:23:16 +0100

    UserProfile test

--- spotify-api-client-test/src/main/java/com/victorlh/spotify/spotifyapiclienttest/controllers/UserProfileController.java
+++ spotify-api-client-test/src/main/java/com/victorlh/spotify/spotifyapiclienttest/controllers/UserProfileController.java
@@ -1,2 +1,28 @@
-package com.victorlh.spotify.spotifyapiclienttest.controllers;public class UserProfileController {
+package com.victorlh.spotify.spotifyapiclienttest.controllers;
+
+import com.victorlh.spotify.apiclient.models.PrivateUserObject;
+import com.victorlh.spotify.apiclient.models.PublicUserObject;
+import com.victorlh.spotify.spotifyapiclienttest.services.UserProfileService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    public UserProfileController(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @GetMapping("/me")
+    private PrivateUserObject getMeUserProfile() {
+        return userProfileService.getMeUserProfile();
+    }
+
+    @GetMapping("/users/{userId}")
+    private PublicUserObject getUserProfile(@PathVariable("userId") String userId) {
+        return userProfileService.getUserProfile(userId);
+    }
 }
--- spotify-api-client-test/src/main/java/com/victorlh/spotify/spotifyapiclienttest/services/impl/UserProfileServiceImpl.java
+++ spotify-api-client-test/src/main/java/com/victorlh/spotify/spotifyapiclienttest/services/impl/UserProfileServiceImpl.java
@@ -1,2 +1,43 @@
-package com.victorlh.spotify.spotifyapiclienttest.services.impl;public class UserProfileServiceImpl {
+package com.victorlh.spotify.spotifyapiclienttest.services.impl;
+
+import com.victorlh.spotify.apiclient.SpotifyApiClient;
+import com.victorlh.spotify.apiclient.exceptions.SpotifyGeneralApiException;
+import com.victorlh.spotify.apiclient.models.PrivateUserObject;
+import com.victorlh.spotify.apiclient.models.PublicUserObject;
+import com.victorlh.spotify.apiclient.services.userprofile.UserProfileApiService;
+import com.victorlh.spotify.spotifyapiclienttest.exceptions.SpotifyApiExceptionRuntime;
+import com.victorlh.spotify.spotifyapiclienttest.services.SpotifyClientService;
+import com.victorlh.spotify.spotifyapiclienttest.services.UserProfileService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileServiceImpl implements UserProfileService {
+
+    private final SpotifyClientService spotifyClientService;
+
+    public UserProfileServiceImpl(SpotifyClientService spotifyClientService) {
+        this.spotifyClientService = spotifyClientService;
+    }
+
+    @Override
+    public PrivateUserObject getMeUserProfile() {
+        SpotifyApiClient spotifyApiClient = spotifyClientService.getSpotifyApiClient();
+        UserProfileApiService userProfileApiService = spotifyApiClient.getUserProfileApiService();
+        try {
+            return userProfileApiService.getMeProfile();
+        } catch (SpotifyGeneralApiException e) {
+            throw new SpotifyApiExceptionRuntime(e);
+        }
+    }
+
+    @Override
+    public PublicUserObject getUserProfile(String userId) {
+        SpotifyApiClient spotifyApiClient = spotifyClientService.getSpotifyApiClient();
+        UserProfileApiService userProfileApiService = spotifyApiClient.getUserProfileApiService();
+        try {
+            return userProfileApiService.getUserProfile(userId);
+        } catch (SpotifyGeneralApiException e) {
+            throw new SpotifyApiExceptionRuntime(e);
+        }
+    }
 }
